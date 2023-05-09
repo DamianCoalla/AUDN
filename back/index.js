8c69ebe1391132ed5063ed241fbc6f1b7a80a52d
Alejo Toriano <alejo.toriano@gmail.com>
2023-05-09 15:53:58 -0300

    Se agregaron rutas y controlers

--- back/index.js
+++ back/index.js
@@ -4,15 +4,20 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 require("dotenv").config();
+const musicRoutes = require("./routes/musicRoutes")
+
 
 //creamos el servidor con express
 const app = express();
 
 //middleware
-/* app.use(morgan("dev"));
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(cors()); */
+app.use(cors());
+
+// colocar rutas
+app.use("/api", musicRoutes)
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
