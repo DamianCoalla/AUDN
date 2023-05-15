d4aab08eeb7adc3e1259d3abc38621278c9357a1
Alejo Toriano <alejo.toriano@gmail.com>
2023-05-15 10:27:22 -0300

    Cambios en archivo JSON

--- front/src/Components/Input/Input.jsx
+++ front/src/Components/Input/Input.jsx
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Input.css";
+import Data from "../../../resources";
 
 function Input(props) {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  function handleSearchChange(event) {
+    setSearchTerm(event.target.value);
+  }
+
+  useEffect(() => {
+    const searchResults = Data.filter((songs) =>
+      // pokemon.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      songs.algo.toString().includes(searchTerm.toLowerCase())
+    );
+    // props.setPokeList(searchResults);
+  }, [searchTerm]);
+  console.log(searchTerm);
+
   return (
     <div>
       <input
         type={`${props.type}`}
         /* placeholder={`${props.placeHolder}` }*/
         className="inputSearchBar"
-        onChange={props.onChange}
+        // onChange={props.onChange,}
+        placeholder="Search"
+        value={searchTerm}
+        onChange={handleSearchChange}
       />
     </div>
   );
